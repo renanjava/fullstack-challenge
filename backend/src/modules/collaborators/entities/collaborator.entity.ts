67ac2cdcfae2bd235dd5098c07b32860ae1a4602
Renan <renancvr777@gmail.com>
2025-10-03 21:19:50 -0300

    refactor: adjust correct files locations

--- backend/src/modules/collaborators/entities/collaborator.entity.ts
+++ backend/src/modules/collaborators/entities/collaborator.entity.ts
@@ -1,1 +1,10 @@
-export class Collaborator {}
+import { Collaborators } from '@prisma/client';
+
+export class CollaboratorsEntity implements Collaborators {
+  id: string;
+  deleted_at: Date | null;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+  user_id: string;
+}
