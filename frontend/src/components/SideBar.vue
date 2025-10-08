f54547bb953906563cd9505b377c5890aa4d1b81
renanjava <renancvr777@gmail.com>
2025-10-08 15:41:11 -0300

    feat: adding home page side bar (wip)

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1 class="title">Time Tracker</h1>
+    <h3 class="subtitle is-6 my-6">Menu principal</h3>
+    <ul class="menu-list">
+      <li><a>Dashboard</a></li>
+      <li><a>Tarefas</a></li>
+      <li><a>Projetos</a></li>
+      <li><a>Usuários</a></li>
+      <li><a>Colaboradores</a></li>
+      <li><a>Relatórios</a></li>
+    </ul>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BarraLateral',
+})
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,11 +1,23 @@
 <script lang="ts">
+import SideBar from '@/components/SideBar.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'HomePage',
+  components: {
+    SideBar,
+  },
 })
 </script>
 
-<template>Home Page...</template>
+<template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css" />
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">teste</div>
+  </main>
+</template>
 
 <style scoped></style>
