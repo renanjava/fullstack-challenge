90bbe6e13924d1b5b9bca062dbd79c8d4a7db5b9
Renan <renancvr777@gmail.com>
2025-10-09 03:17:29 -0300

    refactor(wip): extract list from itens into a component

---
+++ frontend/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box" v-for="(item, index) in list" :key="index">
+    <p class="title">{{ item.name }}</p>
+  </div>
+</template>
+
+<style lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'List',
+  props: {
+    list: {
+      type: Array as PropType<Array<{ id: number; name: string }>>,
+      required: true,
+    },
+  },
+})
+</style>
