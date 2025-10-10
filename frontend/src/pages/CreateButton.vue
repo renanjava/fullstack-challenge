ce980dd114674f20f40a7826794b8f31efecea31
Renan <renancvr777@gmail.com>
2025-10-09 22:57:18 -0300

    feat: adding create button component

---
+++ frontend/src/pages/CreateButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="buttons has-addons is-right">
+    <button class="button is-success mb-4">Criar novo {{ name }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CreateButton',
+  props: {
+    name: { type: String, required: true },
+  },
+})
+</script>
+
+<style scoped></style>
--- frontend/src/pages/ProjectsPage.vue
+++ frontend/src/pages/ProjectsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css" />
   <DefaultMain :primary-text="'Projetos'" :second-text="'Organize seu trabalho em projetos'">
+    <CreateButton :name="name" />
     <List :list="projectsList" />
   </DefaultMain>
 </template>
@@ -11,16 +12,19 @@ import DefaultMain from '@/components/DefaultMain.vue'
 import List from '@/components/List.vue'
 import type { IProjects } from '@/interfaces.ts/projects.interface'
 import { defineComponent } from 'vue'
+import CreateButton from './CreateButton.vue'
 
 export default defineComponent({
   name: 'ProjectsPage',
   components: {
     DefaultMain,
     List,
+    CreateButton,
   },
   data() {
     return {
       projectsList: [] as IProjects[],
+      name: 'projeto',
     }
   },
   async created() {
