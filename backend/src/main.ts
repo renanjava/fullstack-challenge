ce86fd1e490f45e57066b6d2cc8c504def83dbbc
Renan <renancvr777@gmail.com>
2025-10-12 20:38:10 -0300

    docs: adding nestjs swagger integration into back-end to api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack Challenge')
+    .setDescription('Time Tracker API documentation')
+    .setVersion('1.0')
+    .addTag('time-tracker')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
