0480f301952b2febde318fa92f44f067f800e1c2
Renan <renancvr777@gmail.com>
2025-10-12 16:10:40 -0300

    fix: broken test who need mock config service with jwt secret key

--- backend/src/__tests__/app.module.spec.ts
+++ backend/src/__tests__/app.module.spec.ts
@@ -2,14 +2,22 @@ import { AppModule } from './../app.module';
 import { mockPrismaService } from './../prisma/mocks/prisma-service.mock';
 import { PrismaService } from '../prisma/prisma.service';
 import { Test } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppModule', () => {
-  it('should compile AppModule with mocked Prisma', async () => {
+  it('should compile AppModule with Prisma and Jwt mocked', async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     })
       .overrideProvider(PrismaService)
       .useValue(mockPrismaService)
+      .overrideProvider(ConfigService)
+      .useValue({
+        get: jest.fn().mockImplementation((key: string) => {
+          if (key === 'JWT_SECRET') return 'unsecret-jwt-key';
+          return null;
+        }),
+      })
       .compile();
 
     expect(moduleRef).toBeDefined();
